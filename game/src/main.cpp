58a691288277df972b99025b8d825ae10da202f1
insotheo <maxim.insotheo@gmail.com>
2025-04-20 13:40:00 +0400

    Hello, Window!

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-	std::cout << "Hello, World!\n";
+	sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "Hello, World!");
+	window.setVerticalSyncEnabled(true);
+
+	while (window.isOpen()) {
+		while (const auto& event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+		window.clear(sf::Color::White);
+
+		window.display();
+	}
+
 	return 0;
 }
